039dadeb74bebcbdb7e04ba9a9df4342144e4126
Saif-Mahmud-Khan-Dourjoy <60444138+Saif-Mahmud-Khan-Dourjoy@users.noreply.github.com>
2024-08-13 16:34:06 +0600

    new demo video added

--- src/app/(containNav)/demo-video/[videoType]/page.js
+++ src/app/(containNav)/demo-video/[videoType]/page.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Image from "next/image";
 import { FaRegCirclePlay } from "react-icons/fa6";
 import V1 from '/public/Video/V1.png'
+import V2 from '/public/Video/V2.png'
 import ModalVideo from 'react-modal-video';
 import 'react-modal-video/scss/modal-video.scss';
 import EmptyBlog_Image from '/public/Blog/EmptyBlog_Image.png';
@@ -16,7 +17,7 @@ export default function VideoType({ params }) {
       <div className='mt-8'>
         {params.videoType == 'allVideo' &&
           <>
-            <div className='grid md:grid-cols-2 xl:grid-cols-3 gap-8'>
+            <div className='grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-8'>
               <div className='' style={{
                 borderRadius: "10px",
                 background: '#FFF',
@@ -24,7 +25,7 @@ export default function VideoType({ params }) {
               }}>
                 <div className='overflow-hidden' style={{ borderTopLeftRadius: "10px", borderTopRightRadius: '10px' }}>
                   <div className='relative hover:scale-110 transition-all duration-500 cursor-pointer' onClick={() => { setVideoId("hAdT_0O7TI8"); setOpen(true) }}>
-                    <Image src={V1} className='w-full max-h-[300px]' alt="" />
+                    <Image src={V1} className='w-full h-[200px]' alt="" />
                     <div className='bg-gray-400/50 absolute top-0 left-0 right-0 bottom-0'></div>
                     <FaRegCirclePlay className='shadow-2xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all duration-500  text-[50px] hover:text-[60px]' />
                   </div>
@@ -40,6 +41,29 @@ export default function VideoType({ params }) {
 
                 </div>
               </div>
+              <div className='' style={{
+                borderRadius: "10px",
+                background: '#FFF',
+                boxShadow: 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px'
+              }}>
+                <div className='overflow-hidden' style={{ borderTopLeftRadius: "10px", borderTopRightRadius: '10px' }}>
+                  <div className='relative hover:scale-110 transition-all duration-500 cursor-pointer' onClick={() => { setVideoId("JzgyDt9PTPU"); setOpen(true) }}>
+                    <Image src={V2} className='w-full h-[200px]' alt="" />
+                    <div className='bg-gray-400/50 absolute top-0 left-0 right-0 bottom-0'></div>
+                    <FaRegCirclePlay className='shadow-2xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all duration-500  text-[50px] hover:text-[60px]' />
+                  </div>
+                </div>
+
+                <div className='pt-5 px-5 pb-8'>
+                  <div className=' text-xl md:text-2xl font-semibold text-gray-950'>
+                  TS360Web Demo2
+                  </div>
+                  <div className='mt-3 text-base md:text-xl'>
+                  Learn How TS360Web can make automation easier for an organization in Hospitality industry
+                  </div>
+
+                </div>
+              </div>
 
             </div>
             <div className='mt-1'>
@@ -48,7 +72,7 @@ export default function VideoType({ params }) {
           </>
         }
         {params.videoType == 'essential' && <>
-          <div className='grid md:grid-cols-2 xl:grid-cols-3 gap-8'>
+          <div className='grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-8'>
             <div className='' style={{
               borderRadius: "10px",
               background: '#FFF',
@@ -56,7 +80,7 @@ export default function VideoType({ params }) {
             }}>
               <div className='overflow-hidden' style={{ borderTopLeftRadius: "10px", borderTopRightRadius: '10px' }}>
                 <div className='relative hover:scale-110 transition-all duration-500 cursor-pointer' onClick={() => { setVideoId("hAdT_0O7TI8"); setOpen(true) }}>
-                  <Image src={V1} className='w-full max-h-[300px]' alt="" />
+                  <Image src={V1} className='w-full h-[200px]' alt="" />
                   <div className='bg-gray-400/50 absolute top-0 left-0 right-0 bottom-0'></div>
                   <FaRegCirclePlay className='shadow-2xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[50px] hover:text-[60px] transition-all duration-500 ' />
                 </div>
@@ -72,6 +96,29 @@ export default function VideoType({ params }) {
 
               </div>
             </div>
+            <div className='' style={{
+                borderRadius: "10px",
+                background: '#FFF',
+                boxShadow: 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px'
+              }}>
+                <div className='overflow-hidden' style={{ borderTopLeftRadius: "10px", borderTopRightRadius: '10px' }}>
+                  <div className='relative hover:scale-110 transition-all duration-500 cursor-pointer' onClick={() => { setVideoId("JzgyDt9PTPU"); setOpen(true) }}>
+                    <Image src={V2} className='w-full h-[200px]' alt="" />
+                    <div className='bg-gray-400/50 absolute top-0 left-0 right-0 bottom-0'></div>
+                    <FaRegCirclePlay className='shadow-2xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all duration-500  text-[50px] hover:text-[60px]' />
+                  </div>
+                </div>
+
+                <div className='pt-5 px-5 pb-8'>
+                  <div className=' text-xl md:text-2xl font-semibold text-gray-950'>
+                  TS360Web Demo2
+                  </div>
+                  <div className='mt-3 text-base md:text-xl'>
+                  Learn How TS360Web can make automation easier for an organization in Hospitality industry
+                  </div>
+
+                </div>
+              </div>
 
           </div>
           <div className='mt-1'>
@@ -128,12 +175,12 @@ export default function VideoType({ params }) {
 
       </div>
       <ModalVideo
-        channel="youtube"
-        youtube={{ mute: 0, autoplay: 0 }}
-        isOpen={isOpen}
-        videoId={videoId}
-        onClose={() => setOpen(false)}
-      />
+          channel="youtube"
+          youtube={{ autoplay: 1, mute: 0, rel: 0 }}
+          isOpen={isOpen}
+          videoId={videoId}
+          onClose={() => setOpen(false)}
+        />
     </>
   )
 }
