7c0631d21f150b6e141af15900a34fc31604701d
Saif-Mahmud-Khan-Dourjoy <60444138+Saif-Mahmud-Khan-Dourjoy@users.noreply.github.com>
2024-08-19 10:13:18 +0600

    added new demo video

--- src/app/(containNav)/demo-video/[videoType]/page.js
+++ src/app/(containNav)/demo-video/[videoType]/page.js
@@ -1,12 +1,14 @@
 "use client"
-import { useState } from "react";
 import Image from "next/image";
+import { useState } from "react";
 import { FaRegCirclePlay } from "react-icons/fa6";
-import V1 from '/public/Video/V1.png'
-import V2 from '/public/Video/V2.png'
 import ModalVideo from 'react-modal-video';
 import 'react-modal-video/scss/modal-video.scss';
 import EmptyBlog_Image from '/public/Blog/EmptyBlog_Image.png';
+import V1 from '/public/Video/V1.png';
+import V2 from '/public/Video/V2.png';
+import V3 from '/public/Video/V3.png';
+import V4 from '/public/Video/V4.png';
 
 
 export default function VideoType({ params }) {
@@ -65,6 +67,59 @@ export default function VideoType({ params }) {
                 </div>
               </div>
 
+              <div className='' style={{
+                  borderRadius: "10px",
+                  background: '#FFF',
+                  boxShadow: 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px'
+                }}>
+                  <div className='overflow-hidden' style={{ borderTopLeftRadius: "10px", borderTopRightRadius: '10px' }}>
+                    <div className='relative hover:scale-110 transition-all duration-500 cursor-pointer' onClick={() => { setVideoId("d7prgzfBMw0"); setOpen(true) }}>
+                    <Image src={V3} className='w-full h-[200px]' alt="" />
+                      <div className='bg-gray-400/50 absolute top-0 left-0 right-0 bottom-0'></div>
+                      <FaRegCirclePlay className='shadow-2xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all duration-500  text-[50px] hover:text-[60px]' />
+                    </div>
+                  </div>
+
+                  <div className='pt-5 px-5 pb-8'>
+                    <div className=' text-xl md:text-2xl font-semibold text-gray-950'>
+                      TS360Web Demo3
+                    </div>
+                    <div className='mt-3 text-base md:text-xl'>
+                      Learn How TS360Web can make automation easier for an organization in Banking industry
+                    </div>
+
+                  </div>
+                </div>
+
+                <div className='' style={{
+                  borderRadius: "10px",
+                  background: '#FFF',
+                  boxShadow: 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px'
+                }}>
+                  <div className='overflow-hidden' style={{ borderTopLeftRadius: "10px", borderTopRightRadius: '10px' }}>
+                    <div className='relative hover:scale-110 transition-all duration-500 cursor-pointer' onClick={() => { setVideoId("P4VkWHXIAq4"); setOpen(true) }}>
+                    <Image src={V4} className='w-full h-[200px]' alt="" />
+                      <div className='bg-gray-400/50 absolute top-0 left-0 right-0 bottom-0'></div>
+                      <FaRegCirclePlay className='shadow-2xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all duration-500  text-[50px] hover:text-[60px]' />
+                    </div>
+                  </div>
+
+                  <div className='pt-5 px-5 pb-8'>
+                    <div className=' text-xl md:text-2xl font-semibold text-gray-950'>
+                      TS360Web Demo4
+                    </div>
+                    <div className='mt-3 text-base md:text-xl'>
+                      Learn How TS360Web can make automation easier for an organization in Utility industry
+                    </div>
+
+                  </div>
+                </div>
+
+              
+              
+
+
+
             </div>
             <div className='mt-1'>
 
@@ -119,6 +174,53 @@ export default function VideoType({ params }) {
 
                 </div>
               </div>
+              <div className='' style={{
+                  borderRadius: "10px",
+                  background: '#FFF',
+                  boxShadow: 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px'
+                }}>
+                  <div className='overflow-hidden' style={{ borderTopLeftRadius: "10px", borderTopRightRadius: '10px' }}>
+                    <div className='relative hover:scale-110 transition-all duration-500 cursor-pointer' onClick={() => { setVideoId("d7prgzfBMw0"); setOpen(true) }}>
+                    <Image src={V3} className='w-full h-[200px]' alt="" />
+                      <div className='bg-gray-400/50 absolute top-0 left-0 right-0 bottom-0'></div>
+                      <FaRegCirclePlay className='shadow-2xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all duration-500  text-[50px] hover:text-[60px]' />
+                    </div>
+                  </div>
+
+                  <div className='pt-5 px-5 pb-8'>
+                    <div className=' text-xl md:text-2xl font-semibold text-gray-950'>
+                      TS360Web Demo3
+                    </div>
+                    <div className='mt-3 text-base md:text-xl'>
+                      Learn How TS360Web can make automation easier for an organization in Banking industry
+                    </div>
+
+                  </div>
+                </div>
+
+                <div className='' style={{
+                  borderRadius: "10px",
+                  background: '#FFF',
+                  boxShadow: 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px'
+                }}>
+                  <div className='overflow-hidden' style={{ borderTopLeftRadius: "10px", borderTopRightRadius: '10px' }}>
+                    <div className='relative hover:scale-110 transition-all duration-500 cursor-pointer' onClick={() => { setVideoId("P4VkWHXIAq4"); setOpen(true) }}>
+                    <Image src={V4} className='w-full h-[200px]' alt="" />
+                      <div className='bg-gray-400/50 absolute top-0 left-0 right-0 bottom-0'></div>
+                      <FaRegCirclePlay className='shadow-2xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all duration-500  text-[50px] hover:text-[60px]' />
+                    </div>
+                  </div>
+
+                  <div className='pt-5 px-5 pb-8'>
+                    <div className=' text-xl md:text-2xl font-semibold text-gray-950'>
+                      TS360Web Demo4
+                    </div>
+                    <div className='mt-3 text-base md:text-xl'>
+                      Learn How TS360Web can make automation easier for an organization in Utility industry
+                    </div>
+
+                  </div>
+                </div>
 
           </div>
           <div className='mt-1'>
