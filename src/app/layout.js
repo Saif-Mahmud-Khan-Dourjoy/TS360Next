062b6844dd2887db183b52ce51d7df0afce15e4e
Saif-Mahmud-Khan-Dourjoy <60444138+Saif-Mahmud-Khan-Dourjoy@users.noreply.github.com>
2024-08-27 10:29:09 +0600

    update the path of public asset

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import NextAuthSessionProvider from "@/providers/NextAuthSessionProvider";
-import '/public/css/common.css'
+import '../../public/css/common.css'
 
 const inter = Inter({ subsets: ["latin"] });
 
