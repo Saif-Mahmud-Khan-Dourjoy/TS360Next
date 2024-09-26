e36af5648e3f39bc59faec4dc7a5408c04bace97
Saif-Mahmud-Khan-Dourjoy <60444138+Saif-Mahmud-Khan-Dourjoy@users.noreply.github.com>
2024-09-26 10:54:43 +0600

    subscription contact design done

--- src/components/Pricing/FAQ.js
+++ src/components/Pricing/FAQ.js
@@ -1,42 +1,28 @@
 "use client"
 import { GetFAQByCategory } from "@/API/Admin/FAQApi"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { FaPlus } from "react-icons/fa"
 import { FaMinus } from "react-icons/fa"
-
-const Data = [
-  {
-    title: "How do I know which plan is right for me?",
-    content:
-      "Sample Content Sample Content  Sample Content Sample Content Sample Content Sample Content Sample Content Sample Content Sample Content",
-  },
-  {
-    title: "How do I know which plan is right for me 2?",
-    content:
-      "Sample Content Sample Content  Sample Content Sample Content Sample Content Sample Content Sample Content Sample Content Sample Content",
-  },
-  {
-    title: "How do I know which plan is right for me 3?",
-    content: "Sample Content Sample Content  Sample Content",
-  },
-]
+import Loading from "../../../public/Loading.gif"
+import Image from "next/image"
 
 export default function FAQ() {
   const [selected, setSelected] = useState(null)
-  const [faqData, setFAQData] = useState(null)
+  const [faqData, setFAQData] = useState([])
+  const [faqLoader, setFaqLoader] = useState(true)
 
-  useState(() => {
+  useEffect(() => {
+    setFaqLoader(true)
     GetFAQByCategory("PRICING").then((res) => {
-      console.log(res)
       if (res?.[0]) {
-       
+        setFaqLoader(false)
         setFAQData([...res?.[0]])
       } else {
-      
+        setFaqLoader(false)
         setFAQData([])
       }
     })
-  })
+  }, [])
   const toggle = (i) => {
     if (selected == i) {
       setSelected(null)
@@ -49,41 +35,60 @@ export default function FAQ() {
       <div className="text-[#2F2F2F] text-[40px] font-bold text-center">
         Frequently Asked Questions
       </div>
-      <div className="mt-10">
-        {faqData?.map((data, index) => (
-          <div
-            className={`w-[60%] mx-auto border border-[#CBCACA] rounded-md py-4 px-6 ${
-              index > 0 ? "mt-8" : ""
-            }`}
-          >
+      {faqData.length > 0 && !faqLoader && (
+        <div className="mt-10">
+          {faqData?.map((data, index) => (
             <div
-              className="flex justify-between items-center gap-4 "
-              onClick={() => toggle(index)}
-            >
-              <div className="text-[#486681] font-semibold text-lg">
-                {data?.title}
-              </div>
-              <div className="">
-                {selected == index ? (
-                  <FaMinus color="#486681" className="cursor-pointer t" />
-                ) : (
-                  <FaPlus color="#486681" className="cursor-pointer" />
-                )}
-              </div>
-            </div>
-            <div
-              className={` transition-[max-height] duration-500  overflow-hidden ${
-                selected == index ? "max-h-[500px]" : "max-h-0"
+              key={`FAQ-${index}`}
+              className={`w-[60%] mx-auto border border-[#CBCACA] rounded-md py-4 px-6 ${
+                index > 0 ? "mt-8" : ""
               }`}
             >
-              <div className={`mt-2 text-[#8d8b8b]  `}>
-                <div className=" border-t border-gray-300 mb-2" />
-                {data?.content}
+              <div
+                className="flex justify-between items-center gap-4 "
+                onClick={() => toggle(index)}
+              >
+                <div className="text-[#486681] font-semibold text-lg">
+                  {data?.title}
+                </div>
+                <div className="">
+                  {selected == index ? (
+                    <FaMinus color="#486681" className="cursor-pointer t" />
+                  ) : (
+                    <FaPlus color="#486681" className="cursor-pointer" />
+                  )}
+                </div>
+              </div>
+              <div
+                className={` transition-[max-height] duration-500  overflow-hidden ${
+                  selected == index ? "max-h-[500px]" : "max-h-0"
+                }`}
+              >
+                <div className={`mt-2 text-[#8d8b8b]  `}>
+                  <div className=" border-t border-gray-300 mb-2" />
+                  {data?.content}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
+      {faqLoader && (
+        <div className="mt-14">
+          <Image
+            className="w-fit h-fit mx-auto"
+            src={Loading}
+            alt="loading..."
+          />
+        </div>
+      )}
+
+      {faqData.length < 1 && !faqLoader && (
+        <div className="text-gray-500 text-xl text-center mt-10">
+          {" "}
+          No FAQ is Available right now
+        </div>
+      )}
     </div>
   )
 }
